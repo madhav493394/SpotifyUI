54f90663f94fbd56e802bc7dcbb19247384814b4
madhav493394 <madhavsukala18@gmail.com>
2024-01-26 10:11:50 +0530

    some changes made

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log();
+let songs = [
+    {songName: "EnthaChitram", filePath: "song/1.mp3.mp3" , coverPath:"covers/1.jpg"},
+    {songName: "EnthaChitram", filePath: "song/1.mp3.mp3" , coverPath:"covers/1.jpg"},
+    {songName: "EnthaChitram", filePath: "song/1.mp3.mp3" , coverPath:"covers/1.jpg"},
+    {songName: "EnthaChitram", filePath: "song/1.mp3.mp3" , coverPath:"covers/1.jpg"},
+    {songName: "EnthaChitram", filePath: "song/1.mp3.mp3" , coverPath:"covers/1.jpg"},
+    {songName: "EnthaChitram", filePath: "song/1.mp3.mp3" , coverPath:"covers/1.jpg"},
+
+]
+let audioElement = new Audio('1.mp3.mp3');
